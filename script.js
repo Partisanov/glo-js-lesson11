73309f2a840f57c623556e3b35e574b316f9db44
Konstantin Pozharitskiy <tripless@mail.ru>
2022-10-13 14:41:49 +0300

    completed tasks of lesson11

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const btn = document.querySelector('#btn');
+const e_btn = document.querySelector('#e_btn');
+const inputText = document.querySelector('#text');
+const inputRange = document.querySelector('#range');
+const square = document.querySelector('#square');
+const circle = document.querySelector('#circle');
 
 
+btn.addEventListener('click', () => {
+    square.style.backgroundColor = inputText.value;
+});
 
+e_btn.style.display = "none";
 
+inputRange.addEventListener('input', (e) => {
+    const percent = e.target.value + '%';
+    circle.style.width = percent;
+    circle.style.height = percent;
 
+})
